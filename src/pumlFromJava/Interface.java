4f334972555cd1993b2847c017264131c5a17f6e
mekhinini <elies.mekhinini@etu.unistra.fr>
2023-05-16 14:08:27 +0200

    j'ai oublier de changer de branch :(

---
+++ src/pumlFromJava/Interface.java
@@ -0,0 +1,27 @@
+package pumlFromJava;
+
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import java.util.Optional;
+
+import static java.lang.System.lineSeparator;
+
+import javax.lang.model.element.Element;
+
+
+
+public class Interface {
+    Element object;
+    public Interface(Element object){
+        this.object = object;
+    }
+
+    public String toDCA(){
+        String res ="";
+
+
+        return res;
+    }
+
+
+}
